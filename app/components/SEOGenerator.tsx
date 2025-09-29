4b1e066dac485972a497b985fc993d39457ad8ce
Puru <puru@webintensive.com>
2025-09-30 02:55:04 +0530

    FORM UI ISSUE FIXED git add .

--- app/components/SEOGenerator.tsx
+++ app/components/SEOGenerator.tsx
@@ -3,7 +3,7 @@ import React, { ReactElement } from 'react'
 const SEOGenerator = ({children , ariaLabel} : {children : ReactElement , ariaLabel : string}) => {
     return (
         <div
-            className='h-0 w-0 opacity-0 absolute top-0 left-0'
+            className='h-0 w-0 opacity-0 absolute top-0 left-0 overflow-hidden'
             aria-label={ariaLabel}
         >
             {children}
