46d6d91d4d54e125153aa7e8f2631898256bd1d1
Puru <puru@webintensive.com>
2025-11-19 01:00:38 +0530

    BUILD FIX

--- app/utils-components/Footer.tsx
+++ app/utils-components/Footer.tsx
@@ -23,8 +23,8 @@ const Footer = () => {
                     <div className='flex flex-col'>
                         <span className='text-xl'>All Rights reserved ©<span className='bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent'> Bhargava Trading Co. </span></span>
                         <span className='text-sm text-zinc-400 flex flex-row gap-2'>
-                            <a href={`mail:+91 ${mobile}`} className='flex flex-row items-center gap-2'> <FaPhoneAlt /> +91 {mobile} | </a>
-                            <a href={`mail:+91 ${email}`} className='flex flex-row items-center gap-2'> <FaEnvelope />{email} </a>
+                            <a target='_blank' href={`mail:+91 ${mobile}`} className='flex flex-row items-center gap-2'> <FaPhoneAlt /> +91 {mobile} | </a>
+                            <a target='_blank' href={`mail:${email}`} className='flex flex-row items-center gap-2'> <FaEnvelope />{email} </a>
                         </span>
                     </div>
                 </div>
@@ -34,12 +34,12 @@ const Footer = () => {
                 <div className="my-0 text-white font-bold flex flex-col justify-between">
                     <span className='text-xl'>Made with ❤️ by <span className='bg-gradient-to-r from-blue-500 to-pink-500 bg-clip-text text-transparent'> Puru Bhargava</span></span>
                     <div className='flex flex-row gap-2 text-sm text-zinc-400'>
-                        <a href={`mail:+91 ${creatorInfo.mail}`} className='flex flex-row items-center gap-2'> <FaEnvelope /> Email | </a>
-                        <a href={creatorInfo.instagram} className='flex flex-row items-center gap-2'> <FaInstagram /> Instagram | </a>
-                        <a href={creatorInfo.linkedIn} className='flex flex-row items-center gap-2'> <FaLinkedin /> LinkedIn | </a>
-                        <a href={creatorInfo.github} className='flex flex-row items-center gap-2'> <FaGithub /> GitHub | </a>
-                        <a href={`tel:+91 ${creatorInfo.number}`} className='flex flex-row items-center gap-2'> <FaPhoneAlt /> +91 {creatorInfo.number} | </a>
-                        <a href={creatorInfo.portfolio} className='flex flex-row items-center gap-2'> <FaAddressCard /> Portfolio </a>
+                        <a target='_blank' href={`mail:${creatorInfo.mail}`} className='flex flex-row items-center gap-2'> <FaEnvelope /> Email | </a>
+                        <a target='_blank' href={creatorInfo.instagram} className='flex flex-row items-center gap-2'> <FaInstagram /> Instagram | </a>
+                        <a target='_blank' href={creatorInfo.linkedIn} className='flex flex-row items-center gap-2'> <FaLinkedin /> LinkedIn | </a>
+                        <a target='_blank' href={creatorInfo.github} className='flex flex-row items-center gap-2'> <FaGithub /> GitHub | </a>
+                        <a target='_blank' href={`tel:+91 ${creatorInfo.number}`} className='flex flex-row items-center gap-2'> <FaPhoneAlt /> +91 {creatorInfo.number} | </a>
+                        <a target='_blank' href={creatorInfo.portfolio} className='flex flex-row items-center gap-2'> <FaAddressCard /> Portfolio </a>
                     </div>
                 </div>
 
