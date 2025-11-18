8eb53b6dc9fd321b1e3254ac9d6a5e860c147528
Puru <puru@webintensive.com>
2025-11-19 02:22:21 +0530

    UI FIXES

--- app/home/components/hero-components/Contact.tsx
+++ app/home/components/hero-components/Contact.tsx
@@ -4,13 +4,15 @@ import { email, mobile } from "@/app/utils";
 
 const Contact = () => {
     return (
-        <div className="w-full mt-20 flex justify-end items-center gap-4 ">
+        <div className="w-full mt-20 flex justify-end items-center gap-4">
             <a href={`tel:+91${mobile}`} className="px-6 py-3 text-white text-xl rounded-lg font-bold cursor-pointer flex justify-center items-center gap-2 bg-amber-500">
                 <FaPhoneAlt/> Contact Us
             </a>
+
             <a href={`mail:${email}`} className="px-6 py-3 text-white text-xl rounded-lg font-bold cursor-pointer flex justify-center items-center gap-2 bg-amber-500">
                 <MdEmail/> Email Us
             </a>
+
             <a target='_blank' href={`https://wa.me/91${mobile}`} className="px-6 py-3 text-white text-xl rounded-lg font-bold cursor-pointer flex justify-center items-center gap-2 bg-amber-500">
                 <FaWhatsapp/> WhatsApp
             </a>
